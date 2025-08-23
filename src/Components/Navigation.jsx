a4394caee2027c82987268aee00893054f52dd03
Uchenna ogbu <Ucheogbu0070@gmail.com>
2025-08-23 09:16:53 +0300

    Navigation styled for multiple devices

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -8,83 +8,80 @@ const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <>
-      <header className={Styles.header}>
-        <nav className={Styles.nav}>
-          <div className={Styles.logo}>
-            <img src="/Logo.jpg" alt="Little Lemon Logo" />
-          </div>
+    <header className={Styles.header}>
+      <nav className={Styles.nav}>
+        <div className={Styles.logo}>
+          <img src="/Logo.jpg" alt="Little Lemon Logo" />
+        </div>
 
-          <ul className={`${Styles.menu} ${isMenuOpen ? Styles.show : ""}`}>
-            <li>
-              <Link
-                to="/"
-                className={Styles.link}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                HOME
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/About"
-                className={Styles.link}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                ABOUT
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/Menu"
-                className={Styles.link}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                MENU
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/Appointment"
-                className={Styles.link}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                APPOINTMENT
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/Appointment"
-                className={Styles.order}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                APPOINTMENT{" "}
-              </Link>
-            </li>
-          </ul>
-
-          {!isMenuOpen ? (
-            <div
-              className={Styles.openmenu}
-              onClick={() => setIsMenuOpen(true)}
-              role="button"
-              tabIndex={0}
-              aria-label="Open menu"
-              onKeyDown={(e) => e.key === "Enter" && setIsMenuOpen(true)}
-            >
-              <GiHamburgerMenu />
-            </div>
-          ) : (
-            <div
-              className={Styles.closemenu}
+        <ul className={`${Styles.menu} ${isMenuOpen ? Styles.show : ""}`}>
+          <li>
+            <Link
+              to="/"
+              className={Styles.link}
               onClick={() => setIsMenuOpen(false)}
             >
-              <MdCancel />
-            </div>
-          )}
-        </nav>
-      </header>
-    </>
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/About"
+              className={Styles.link}
+              onClick={() => setIsMenuOpen(false)}
+            >
+              ABOUT
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/Services"
+              className={Styles.link}
+              onClick={() => setIsMenuOpen(false)}
+            >
+              SERVICES
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              to="/Gallery"
+              className={Styles.link}
+              onClick={() => setIsMenuOpen(false)}
+            >
+              GALLERY
+            </Link>
+          </li>
+        </ul>
+        <Link
+          to="/Appointment"
+          className={Styles.order}
+          onClick={() => setIsMenuOpen(false)}
+        >
+          APPOINTMENT{" "}
+        </Link>
+
+        {!isMenuOpen ? (
+          <div
+            className={Styles.openmenu}
+            onClick={() => setIsMenuOpen(true)}
+            role="button"
+            tabIndex={0}
+            aria-label="Open menu"
+            onKeyDown={(e) => e.key === "Enter" && setIsMenuOpen(true)}
+          >
+            <GiHamburgerMenu />
+          </div>
+        ) : (
+          <div
+            className={Styles.closemenu}
+            onClick={() => setIsMenuOpen(false)}
+          >
+            <MdCancel />
+          </div>
+        )}
+      </nav>
+    </header>
   );
 };
 
