7e5dfd41cac8fefebde641d08431930ceed6fa7e
Uchenna ogbu <Ucheogbu0070@gmail.com>
2025-08-27 07:38:08 +0300

    framer motion added and stylist section added

--- src/Pagesection/Ourservices.jsx
+++ src/Pagesection/Ourservices.jsx
@@ -1,39 +1,53 @@
 import React from "react";
 import Style from "../Css/Ourservices.module.css";
+import { FaPaintbrush } from "react-icons/fa6";
+import { PiHairDryer } from "react-icons/pi";
+import { GiRazor } from "react-icons/gi";
+import { TbRazorElectric } from "react-icons/tb";
+import Stylist from "./Stylist";
 
 const Ourservices = () => {
   const services = [
     {
+      icon: <GiRazor />,
       title: "Trimming",
       description: "Precision trims to keep your look sharp and fresh.",
     },
     {
+      icon: <TbRazorElectric />,
       title: "HairStyle",
       description: "Modern and classic styles tailored to your vibe.",
     },
+
     {
-      title: "Shaving",
-      description: "Clean shaves and beard sculpting with finesse.",
-    },
-    {
+      icon: <FaPaintbrush />,
       title: "Facials",
       description: "Rejuvenating treatments for glowing, healthy skin.",
+    },
+    {
+      icon: <PiHairDryer />,
+      title: "Hair coloring",
+      description: "Coloring of hair and treatment of different kind",
     },
   ];
 
   return (
-    <section className={Style.container}>
-      <h2 className={Style.heading}>Our Services</h2>
-      <div className={Style.grid}>
-        {services.map((service, index) => (
-          <div className={Style.card} key={index}>
-            <img src="" alt={service.title} />
-            <h3>{service.title}</h3>
-            <p>{service.description}</p>
-          </div>
-        ))}
-      </div>
-    </section>
+    <>
+      <section className={Style.container}>
+        <h2 className={Style.heading}>Our Services</h2>
+        <div className={Style.grid}>
+          {services.map((service, index) => (
+            <div className={Style.card} key={index}>
+              <div className={Style.icon}>{service.icon}</div>
+              <h3>{service.title}</h3>
+              <p>{service.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <hr />
+      <Stylist />
+    </>
   );
 };
 
---
+++ src/Pagesection/Stylist.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Style from "../Css/Stylist.module.css";
+import { motion } from "framer-motion";
+
+// Stylist information array list.
+
+const stylists = [
+  {
+    name: "Jayden Cole",
+    bio: "Expert in modern fades and beard sculpting.",
+    img: "/Artist.jpg",
+  },
+  {
+    name: "Amira Khan",
+    bio: "Specializes in natural hair care and styling.",
+    img: "/Artist4.jpg",
+  },
+  {
+    name: "Leo Tanaka",
+    bio: "Creative colorist with a flair for bold transformations.",
+    img: "/Artist5.png",
+  },
+];
+
+const Stylist = () => {
+  return (
+    //  header container starts here
+
+    <section className={Style.container}>
+      <h4 className={Style.subheading}>Expert Stylists</h4>
+      <p className={Style.heading}>
+        Our stylist offers traditional and contemporary services along with our
+        very own treatment.
+      </p>
+
+      {/* looping the array information to get artist details */}
+
+      <div className={Style.grid}>
+        {stylists.map((stylist, index) => (
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.3 }}
+            transition={{ duration: 0.8, delay: index * 0.2 }}
+            className={Style.card}
+            key={index}
+          >
+            <img src={stylist.img} alt={stylist.name} className={Style.image} />
+            <h4>{stylist.name}</h4>
+            <p>{stylist.bio}</p>
+          </motion.div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Stylist;
