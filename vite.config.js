ad562c683aadb813f7d8d74d237eeedfeea7755e
Uchenna ogbu <ogbu41@gmail.com>
2025-09-27 19:25:57 +0300

    routing loading fixed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import legacy from "@vitejs/plugin-legacy";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import legacy from "@vitejs/plugin-legacy";
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     legacy({
+//       targets: ["defaults", "not IE 11", "Safari >= 12"],
+//       modernPolyfills: true,
+//       additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+//     }),
+//   ],
+//   build: {
+//     minify: "terser",
+//     chunkSizeWarningLimit: 1000,
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes("node_modules")) {
+//             return id.toString().split("node_modules/")[1].split("/")[0];
+//           }
+//         },
+//       },
+//     },
+//   },
+// });
 
 export default defineConfig({
   plugins: [
@@ -11,6 +35,9 @@ export default defineConfig({
       additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
     }),
   ],
+  server: {
+    historyApiFallback: true, // 👈 Add this line
+  },
   build: {
     minify: "terser",
     chunkSizeWarningLimit: 1000,
