4a053455e446145fd691d36e60f5ec6801d898ae
Uchenna ogbu <ogbu41@gmail.com>
2025-09-28 06:32:39 +0300

    fixing vite.config bugs

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,9 @@
 //     },
 //   },
 // });
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 export default defineConfig({
   plugins: [
     react(),
